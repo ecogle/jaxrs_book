f22a562caa9aa2bef51c89b55efd51816823f1b5
Chad Ogle <ecogle@c3devloping.com>
2018-03-06 05:23:47 -0600

    Changed the DepartmentService entity to match the book.

--- src/main/java/cokm/packtpub/rest/ch3/service/DepartmentService.java
+++ src/main/java/cokm/packtpub/rest/ch3/service/DepartmentService.java
@@ -1,13 +1,32 @@
 package cokm.packtpub.rest.ch3.service;
 
+import cokm.packtpub.rest.ch3.model.Departments;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaQuery;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("departments")
+@Stateless
 public class DepartmentService {
 
+    @PersistenceContext(unitName = "com.packtpub_rest-chapter3-service_war_1.0-SNAPSHOTPU")
+    private EntityManager entityManager;
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Departments> findAllDepartments(){
+        CriteriaQuery cq = entityManager.getCriteriaBuilder().createQuery();
+        cq.select(cq.from(Departments.class));
+        List<Departments> departments = entityManager.createQuery(cq).getResultList();
+        return departments;
+    }
+    
     @GET
     @Path("hello")
     @Produces(MediaType.APPLICATION_JSON)
